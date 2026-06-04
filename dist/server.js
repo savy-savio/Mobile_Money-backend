345bad382dd5a123e251ef5cf37493683096f90e
Okwoli <okwolig60@gmail.com>
2026-06-04 14:08:21 +0100

    update

--- dist/server.js
+++ dist/server.js
@@ -3,9 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const dotenv_1 = __importDefault(require("dotenv"));
+// import dotenv from 'dotenv';
 const database_1 = require("./config/database");
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const settingsRoutes_1 = __importDefault(require("./routes/settingsRoutes"));
